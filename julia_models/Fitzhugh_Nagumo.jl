c754739e1fe79a3b024772a036155b6f587375c5
Pavan Chaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2020-08-02 17:44:06 +0100

    changing paramters

--- julia_models/Fitzhugh_Nagumo.jl
+++ julia_models/Fitzhugh_Nagumo.jl
@@ -9,9 +9,9 @@ w = 1.0
 
 a = 0.7
 b = 0.8
+I = 0.34
+R = 1.0
 τ = 10
-I = 0.35
-
 
 tspan = (0.0, 100.0)
 
@@ -21,17 +21,17 @@ tspan = (0.0, 100.0)
 function fitzhugh_nagumo!(du, u, p, t)
 
     v, w = u
-    a, b, I = p
+    a, b, I, R, τ = p
 
 
-    du[1] = v - w  * I - (w^3)/3
+    du[1] = v - (v^3)/3 - w  + R * I
 
     du[2] = (v + a - (b * w)) / τ
 end
 
 u0 = [v, w]
-p = [a, b, I]
+p = [a, b, I, R, τ]
 prob = ODEProblem(fitzhugh_nagumo!,u0,tspan,p)
 sol = solve(prob)
 
-plot(sol, vars =1)
+plot(sol, vars =(1,2))
