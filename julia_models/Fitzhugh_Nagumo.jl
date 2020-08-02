89ffc6773838a5e5b8e6d0fd577af8b4135764cc
Pavan Chaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2020-08-02 20:16:45 +0100

    adding uncertainty analysis into script, updating manifest with DiffEqUncertainty

--- julia_models/Fitzhugh_Nagumo.jl
+++ julia_models/Fitzhugh_Nagumo.jl
@@ -1,20 +1,6 @@
 using Plots
 using DifferentialEquations
-
-#----------------------------------------------
-# Set Parameters:
-
-v = -2.0
-w = 1.0
-
-a = 0.7
-b = 0.8
-I = 0.34
-R = 1.0
-τ = 10
-
-tspan = (0.0, 100.0)
-
+using DiffEqUncertainty
 #----------------------------------------------
 # Specify and Solve model:
 
@@ -29,9 +15,21 @@ function fitzhugh_nagumo!(du, u, p, t)
     du[2] = (v + a - (b * w)) / τ
 end
 
-u0 = [v, w]
-p = [a, b, I, R, τ]
+u0 = [-2.0, 1.0]
+p = [0.7, 0.8, 0.34, 1.0, 10]
+tspan = (0.0, 100.0)
 prob = ODEProblem(fitzhugh_nagumo!,u0,tspan,p)
 sol = solve(prob)
 
-plot(sol, vars =(1,2))
+plot(sol, vars =(1))
+
+
+cb = ProbIntsUncertainty(0.1,1)
+cb2 = ProbIntsUncertainty(0.05,1)
+ensemble_prob = EnsembleProblem(prob)
+sim = solve(ensemble_prob,Euler(),trajectories=100,callback=cb,dt=1/10)
+sim2 = solve(ensemble_prob,Euler(),trajectories=50,callback=cb2,dt=1/100)
+
+plot(sim, vars=(0,1), color="grey")
+plot!(sim2, vars=(0,1), color="lightsalmon2")
+plot!(sol, vars =(1), color="red3", linewidth =3)
